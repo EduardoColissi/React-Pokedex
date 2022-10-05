457fdf83c9dce857d5ab281a45285492e126371d
EduardoColissi <educolissi@hotmail.com>
2022-10-05 19:42:52 -0300

    :sparkles: feat: bringing pokemon infos

--- pokedex/src/pokedex/Pokedex.tsx
+++ pokedex/src/pokedex/Pokedex.tsx
@@ -1,27 +1,50 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 interface PokedexProps {}
 
-const array: string[] = ["Pikachu", "Raichu"];
+interface PokemonListInterface {
+  name: string;
+  url: string;
+}
 
 export const Pokedex: React.FC<PokedexProps> = () => {
-  const [pokemons, setPokemons] = useState<string[]>(array);
-  const [selectedPokemon, setSelectedPokemon] = useState<string | undefined>(
-    undefined
-  );
+  const [pokemons, setPokemons] = useState<PokemonListInterface[]>([]);
+  const [selectedPokemon, setSelectedPokemon] = useState<
+    PokemonListInterface | undefined
+  >(undefined);
+  const [selectedPokemonInfos, setSelectedPokemonInfos] = useState<
+    any | undefined
+  >(undefined);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => setPokemons(response.data.results));
+  }, []);
+
+  useEffect(() => {
+    if (!selectedPokemon) return;
+
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon.name}`)
+      .then((response) => setSelectedPokemonInfos(response.data));
+  }, [selectedPokemon]);
 
   return (
     <div>
       <h1>Pokedex</h1>
-      <h1>Pokemons:</h1>
+      Pokemons:
       {pokemons.map((pokemon) => (
-        <button onClick={() => setSelectedPokemon(pokemon)}>{pokemon}</button>
+        <button onClick={() => setSelectedPokemon(pokemon)}>
+          {pokemon.name}
+        </button>
       ))}
+      ;
       <h2>
-        Pokemon Selecionado:
-        {selectedPokemon ? selectedPokemon : "Nenhum selecionado."}
+        Pokemon Selecionado: {selectedPokemon?.name || "Nenhum selecionado."}
       </h2>
+      {JSON.stringify(selectedPokemonInfos, undefined, 2)}
     </div>
   );
 };
